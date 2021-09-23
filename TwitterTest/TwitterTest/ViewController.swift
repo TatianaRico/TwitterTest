d7acb722c305565255895de2238de2ba30a733ee
Tatiana Rico <tatianarico1702@gmail.com>
2021-09-23 19:28:15 -0300

    criado o projeto

---
+++ TwitterTest/TwitterTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterTest
+//
+//  Created by Tatiana Rico on 23/09/21.
+//  Copyright © 2021 Tatiana Rico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
