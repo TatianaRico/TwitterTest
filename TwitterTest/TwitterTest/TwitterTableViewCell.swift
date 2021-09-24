66ccd029377d5c29a750064bea92501c884294bf
Tatiana Rico <tatianarico1702@gmail.com>
2021-09-24 16:15:44 -0300

    removendo main.storyboard

---
+++ TwitterTest/TwitterTest/TwitterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  twitterTableViewCell.swift
+//  TwitterTest
+//
+//  Created by Tatiana Rico on 24/09/21.
+//  Copyright © 2021 Tatiana Rico. All rights reserved.
+//
+
+import UIKit
+
+class twitterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
